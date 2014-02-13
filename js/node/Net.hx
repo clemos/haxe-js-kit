057822f9008ffd7ee1407f05252a146fa8a35255
Peekmo <axel.anceau@gmail.com>
2014-02-13 23:20:46 +0100

    Net, Socket & Server classes updated

--- js/node/Net.hx
+++ js/node/Net.hx
@@ -1,5 +1,24 @@
 package js.node;
 
+// NetConnectionOpt replaced by TPCSocketOptions
+typedef TCPSocketOptions = {
+  var port: Int;
+  @:optional var host: String;
+  @:optional var localAddress: String;
+  @:optional var allowHalfOpen: Bool;
+}
+
+typedef UNIXsocketOptions = {
+  var path: String;
+  @:optional var allowHalfOpen: Bool;
+}
+ 
+typedef Address = {
+  var port: Int;
+  var family: String;
+  var address: String;
+}
+
 /* NET ............................................. */
   
 /* 
@@ -11,19 +30,17 @@ extends js.node.events.EventEmitter
 implements npm.Package.Require<"net","*"> 
 {
   static function createServer(?options:{allowHalfOpen:Bool},fn:js.node.net.Socket->Void):js.node.net.Server;
-  @:overload(function(cs:String):js.node.net.Socket {})
-  static function createConnection(port:Int,host:String):js.node.net.Socket;
-  @:overload(function(cs:String):js.node.net.Socket {})
-  static function connect(port:Int,host:String):js.node.net.Socket;                    
+
+  @:overload(function(path: String, ?connectListener: Void->Void): js.node.net.Socket {}) // Unix socket
+  @:overload(function(options: Dynamic, ?connectListener: Void->Void): js.node.net.Socket {}) // options is TPCSocket or UNIXSocket
+  static function createConnection(port:Int, ?host:String, ?connectListener: Void->Void):js.node.net.Socket; // TCP Socket
+
+  @:overload(function(path: String, ?connectListener: Void->Void): js.node.net.Socket {}) // Unix socket
+  @:overload(function(options: Dynamic, ?connectListener: Void->Void): js.node.net.Socket {}) // options is TPCSocket or UNIXSocket
+  static function connect(port:Int, ?host:String, ?connectListener: Void->Void):js.node.net.Socket;
+
   static function isIP(input:String):Int; // 4 or 6
   static function isIPv4(input:String):Bool;
   static function isIPv6(input:String):Bool;
-}
-
-
-typedef NetConnectionOpt = {
-    port:Int,
-    ?host:String,
-    ?localAddress:String
 }
 
--- js/node/net/Socket.hx
+++ js/node/net/Socket.hx
@@ -1,6 +1,14 @@
 package js.node.net;
 
 import js.node.Net;
+
+typedef SocketOptions = {
+  @:optional var fd: Int;
+  @:optional var allowHalfOpen: Bool;
+  @:optional var readable: Bool;
+  @:optional var writable: Bool;
+}
+
 /*
   
   Emits:
@@ -12,27 +20,37 @@ extern class Socket
 implements npm.Package.RequireNamespace<"net","*">
 extends js.node.events.EventEmitter 
 {
-  var remoteAddress:String;
-  var remotePort:Int;
-  var bufferSize:Int;
-  var bytesRead:Int;
-  var bytesWritten:Int;
-                          
-  @:overload(function(path:String,?cb:Void->Void):Void {})
-  @:overload(function(options:NetConnectionOpt,connectionListener:Void->Void):Void {})
-  function connect(port:Int,?host:String,?cb:Void->Void):Void;
-  function setEncoding(enc:String):Void;
-  function setSecure():Void;
-  @:overload(function(data:Dynamic,?enc:String,?fileDesc:Int,?cb:Void->Void):Bool {})
-  function write(data:Dynamic,?enc:String,?cb:Void->Void):Bool;
-  function end(?data:Dynamic,?enc:String):Void;
-  function destroy():Void;
-  function pause():Void;
-  function resume():Void;
-  function setTimeout(timeout:Int,?cb:Void->Void):Void;
-  function setNoDelay(?noDelay:Bool):Void;
-  function setKeepAlive(enable:Bool,?delay:Int):Void;
-  function address():{address:String,port:Int}; 
-  function new(?options:Dynamic):Void;
+    public static inline var EVENT_TIMEOUT = "timeout";
+    public static inline var EVENT_CONNECT = "connect";
+    public static inline var EVENT_CLOSE = "close";
+    public static inline var EVENT_ERROR = "error";
+    public static inline var EVENT_DRAIN = "drain";
+    public static inline var EVENT_DATA = "data";
+    public static inline var EVENT_END = "end";
+    
+    var remoteAddress:String;
+    var remotePort:Int;
+    var localAddress: String;
+    var localPort: Int;
+    var bufferSize:Int;
+    var bytesRead:Int;
+    var bytesWritten:Int;
+                            
+    @:overload(function(path:String,?connectListener:Void->Void):Void {})
+    function connect(port:Int,?host:String,?connectListener:Void->Void):Void;
 
+    function setEncoding(?encoding:String):Void;
+    function setSecure():Void;
+    function write(data:Dynamic,?encoding:String,?callback:Void->Void):Bool;
+    function end(?data:Dynamic,?encoding:String):Void;
+    function destroy():Void;
+    function pause():Void;
+    function resume():Void;
+    function setTimeout(timeout:Int,?callback:Void->Void):Void;
+    function setNoDelay(?noDelay:Bool):Void;
+    function setKeepAlive(?enable:Bool,?delay:Int):Void;
+    function address():js.node.Address;
+    function unref(): Void;
+    function ref(): Void;
+    function new(?options:SocketOptions):Void;
 }
