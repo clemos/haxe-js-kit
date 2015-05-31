784419caaa7c7683d9d56eaedf2f64af6a1fe17f
Andreas <ciscoheat@gmail.com>
2015-05-31 15:50:41 +0200

    Updated js/Lib.hx with Haxe 3.2.0 improvements.

--- js/Lib.hx
+++ js/Lib.hx
@@ -23,19 +23,10 @@ package js;
 
 class Lib {
 
+	////////// js-kit additions //////////
+
 	public static var arguments (get,null) : Array<Dynamic>;
 	public static var self (get,null) : Dynamic;
-
-	/**
-		Inserts a 'debugger' statement that will make a breakpoint if a debugger is available.
-	**/
-	public static inline function debug() {
-		untyped __js__("debugger");
-	}
-
-	public static inline function eval( code : String ) : Dynamic {
-		return untyped __js__("eval")(code);
-	}
 
 	static inline function get_arguments(){
 		return untyped __js__("arguments");
@@ -45,4 +36,65 @@ class Lib {
 		return untyped __js__("this");
 	}
 
+	////////// Original haxe/std/js/Lib.hx code (3.2.0) //////////
+
+	/**
+		Inserts a 'debugger' statement that will make a breakpoint if a debugger is available.
+	**/
+	public static inline function debug() {
+		untyped __js__("debugger");
+	}
+
+	/**
+		Display an alert message box containing the given message.
+		@deprecated Use Browser.alert() instead.
+	**/
+	@:deprecated("Lib.alert() is deprecated, use Browser.alert() instead")
+	public static function alert( v : Dynamic ) {
+		untyped __js__("alert")(js.Boot.__string_rec(v,""));
+	}
+
+	public static inline function eval( code : String ) : Dynamic {
+		return untyped __js__("eval")(code);
+	}
+
+	/**
+		Inserts a `require` expression that loads JavaScript object from
+		a module or file specified in the `module` argument.
+
+		This is only supported in environments where `require` function
+		is available, such as Node.js or RequireJS.
+	**/
+	public static inline function require( module:String ) : Dynamic {
+		return untyped __js__("require")(module);
+	}
+
+	/**
+		Returns JavaScript `undefined` value.
+
+		Note that this is only needed in very rare cases when working with external JavaScript code.
+
+		In Haxe, `null` is used to represent the absence of a value.
+	**/
+	public static var undefined(get,never) : Dynamic;
+	static inline function get_undefined() : Dynamic {
+		return untyped __js__("undefined");
+	}
+
+	/**
+		`nativeThis` is the JavaScript `this`, which is semantically different
+		from the Haxe `this`. Use `nativeThis` only when working with external
+		JavaScript code.
+
+		In Haxe, `this` is always bound to a class instance.
+		In JavaScript, `this` in a function can be bound to an arbitrary
+		variable when the function is called using `func.call(thisObj, ...)` or
+		`func.apply(thisObj, [...])`.
+
+		Read more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+	**/
+	public static var nativeThis(get,never) : Dynamic;
+	@:extern static inline function get_nativeThis() : Dynamic {
+		return untyped __js__("this");
+	}
 }
